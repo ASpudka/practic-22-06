0473a9e9c79eb0d3ea7f733fc3b40f001f8a6ea9
Aleksandra <aspudka@gmail.com>
2022-06-24 20:09:24 +0300

    Example Stack

---
+++ Stack.js
@@ -0,0 +1,42 @@
+"use strict";
+
+class Stack {
+  constructor(maxSize = 10,...args) {
+    this._maxSize = maxSize;
+    this._size = 0;
+    for (const argument of args) {
+      this.push(argument);
+    }
+  };
+  get size() {
+    return this._size;
+  };
+  get empty() {
+    return this._size === 0;
+  };
+  push(argument) {
+    if (this._size >= this._maxSize) {
+      throw new RangeError("stack overflow");
+    }
+    this[`_${this._size}`] = argument;
+    return ++this._size;
+  }
+  pick() {
+    return this[`_${this._size-1}`];
+  }
+  pop() {
+    if(this.empty){
+      return;
+    }
+    const last = this[`_${this._size-1}`];
+    delete this[`_${this._size-1}`];
+    --this._size;
+    return last;
+  };
+  }
+
+
+const stack1 = new Stack();
+
+const stack2 = new Stack (5,2,3,4,5,6,6);
+console.log(stack2);
